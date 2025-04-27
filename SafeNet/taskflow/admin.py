b56c4d4b2e2e012efdad9d0981cabb4db6244ae4
Atharv <111235745+AtharvK17@users.noreply.github.com>
2025-04-27 16:19:44 +0530

    Delete SafeNet directory

--- SafeNet/taskflow/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Project, Task, CSVFile
-
-admin.site.register(Project)
-admin.site.register(Task)
-@admin.register(CSVFile)
-class CSVFileAdmin(admin.ModelAdmin):
-    list_display = ('file', 'uploaded_at')
--- SafeNet/taskflow/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TaskflowConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'taskflow'
